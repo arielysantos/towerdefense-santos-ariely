7669e25b62663c07a472495ced29c644b434238f
arielysantos <arielys923@gmail.com>
2024-10-29 15:18:21 -0300

    inicio do metodo enemyMoviments

---
+++ towerdefense-Ariely-santos/Assets/enemyMoviments.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class enemyMoviments : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+}
