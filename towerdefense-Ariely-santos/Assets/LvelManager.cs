ba290d613c01c6a153fd41abbb9573d6c55dcba4
arielysantos <arielys923@gmail.com>
2024-10-29 16:19:39 -0300

    mais atualizaçoes do metodo levelManager

--- towerdefense-Ariely-santos/Assets/LvelManager.cs
+++ towerdefense-Ariely-santos/Assets/LvelManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LvelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     public static LevelManager main;
 
