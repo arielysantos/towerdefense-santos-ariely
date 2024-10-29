2a4ce8f5be24318ebed3fdf460dcfac24a8b0eec
arielysantos <arielys923@gmail.com>
2024-10-29 16:30:43 -0300

    modificação do metodo enemyMoviments

--- towerdefense-Ariely-santos/Assets/enemyMoviments.cs
+++ towerdefense-Ariely-santos/Assets/enemyMoviments.cs
@@ -42,5 +42,6 @@ public class enemyMoviments : MonoBehaviour
     {
         Vector2 direction = (target.position - transform.position).normalized;
 
+        rb.velocity = direction * moveSpeed;
     }
 }
