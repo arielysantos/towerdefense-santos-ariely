18707d53582f1ab23cafd57ef9b47621d178fc8d
arielysantos <arielys923@gmail.com>
2024-10-31 08:18:26 -0300

    inicio do metodo enemySpawn

---
+++ towerdefense-Ariely-santos/Assets/enemyspanw.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyspanw : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBotweenWaves = 5f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+
+}
